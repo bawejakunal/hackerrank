21bd74e497a19ae52b9e71d6afb0c287628c84c8
Kunal Baweja <bawejakunal15@gmail.com>
2017-08-14 17:14:49 -0400

    modify prime generate

--- generate-primes.py
+++ generate-primes.py
@@ -1,29 +1,34 @@
 import math
 
-def is_prime(primes, num):
-  if num < 2:
-    return False
-  sqrt = math.ceil(math.sqrt(num))
-  for p in primes:
-    if p > sqrt:
-      break
-    if num % p == 0:
-      return False
-  return True
+def insert_primes(num, primes=list()):
+    def is_prime(primes, num):
+        if num < 2:
+            return False
+        sqrt = math.ceil(math.sqrt(num))
+        for p in primes:
+            if p > sqrt:
+                break
+            if num % p == 0:
+                return False
+        return True
 
-def insert_primes(primes, num):
-  if len(primes) > 0 and primes[-1] >= num:
-    return
-  strt = primes[-1] + 1 if len(primes) > 0 else 1
-  for i in range(strt, num + 1):
-    if is_prime(primes, i):
-      primes.append(i)
-  return
+    if len(primes) > 0 and primes[-1] >= num:
+        return primes
+    strt = primes[-1] + 1 if len(primes) > 0 else 1
+    for i in range(strt, num + 1):
+        if is_prime(primes, i):
+            primes.append(i)
+    return primes
 
+
+# build a given list
 primes = list()
-insert_primes(primes, 10)
+insert_primes(10, primes)
 print(primes)
-insert_primes(primes, 11)
+insert_primes(11, primes)
 print(primes)
-insert_primes(primes, 12)
+insert_primes(12, primes)
 print(primes)
+
+# generate a new list
+print(insert_primes(50))
